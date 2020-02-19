70a45d4895b749027673aad737d3eb4908702fc3
Oskar Jonassen <ojonassen@live.dk>
2020-02-19 15:53:47 +0100

    uhud

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("med");
+        System.out.println("dig");
     }
 
 
