8dcd041d51ed6909fce35224905f011855197eb1
ami <amihaiwow@gmail.com>
2020-02-19 15:50:34 +0100

    wong

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,10 @@
 package com.company;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hej");
+        System.out.println("heeeej");
+        System.out.println("somtinwong");
     }
-    System.out.println("heeeej");
-
 }
