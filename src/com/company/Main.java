8d37405deabdc58fced4678ca9e4aab42c19dcc7
Sanne <s145990@student.dtu.dk>
2020-02-19 15:44:01 +0100

    hejhej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hej");
     }
+    System.out.println("heeeej");
+
 }
