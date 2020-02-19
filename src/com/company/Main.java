6dc3ce13e04fa34e4d707de144887a7847174850
kevinbpedersen1 <61202791+kevinbpedersen1@users.noreply.github.com>
2020-02-19 15:48:24 +0100

    med

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hej");
+        System.out.println("med");
     }
-    System.out.println("heeeej");
+
 
 }
+
